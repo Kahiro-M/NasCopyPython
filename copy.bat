ff70d0126d6cf6b1fdcfa82f47b39083e21364b7
Kahiro-M <0204.kahiro@gmail.com>
2023-10-11 09:41:47 +0900

    NASコピーバッチ実行時にフォルダをバッチ保存ディレクトリに移動する処理を追加

--- copy.bat
+++ copy.bat
@@ -3,3 +3,8 @@
 
 @REM 例)\\198.51.100.10\画像\202309\*.jpgを4週間前まで\\203.0.113.110\画像\2023\09\*.jpgとしてコピー
 @REM NasCopy.exe \\198.51.100.10\画像\202309 jpg,bmp,png 4 \\203.0.113.110\画像\2023\09
+
+@echo off
+setlocal enabledelayedexpansion
+cd %~dp0
+
